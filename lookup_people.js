a361a06e699ed911e8a6bf90e1be5196e7d4dd6c
99hsueh <99hhsueh@gmail.com>
2017-02-28 21:39:09 +0000

    connect thru pg and make queries

---
+++ lookup_people.js
@@ -0,0 +1,24 @@
+const pg = require("pg");
+const settings = require("./settings.json"); // settings.json
+
+const client = new pg.Client({
+  user     : settings.user,
+  password : settings.password,
+  database : settings.database,
+  host     : settings.hostname,
+  port     : settings.port,
+  ssl      : settings.ssl
+});
+let input = process.argv[2];
+client.connect((err) => {
+  if (err) {
+    return console.error("Connection Error", err);
+  }
+  client.query("SELECT * FROM famous_people WHERE last_name=$1 OR first_name = $1", [input], (err, result) => {
+    if (err) {
+      return console.error("error running query", err);
+    }
+    console.log(`-${result.rows[0].id}: ${result.rows[0].first_name} ${result.rows[0].last_name}, born ${result.rows[0].birthdate}`);
+    client.end();
+  });
+});
